9437aeef8a03346a43d4df65e9a0cd26f41ab1ca
flpaugust <felipe-2005@outlook.com>
2025-04-15 11:16:54 -0300

    lista menu

--- pizza_menu/lib/menu.dart
+++ pizza_menu/lib/menu.dart
@@ -9,18 +9,26 @@ class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Pizza Menu"),
+      appBar: AppBar(
+        title: Text("Pizza Menu"),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: _buildPizzas(),
+          ),
         ),
-        body: SingleChildScrollView(
-            child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(children: [
-                  Item(pizza: pizzaData[0]),
-                  Item(pizza: pizzaData[2]),
-                  Item(pizza: pizzaData[3]),
-                  Item(pizza: pizzaData[4]),
-                  Item(pizza: pizzaData[5]),
-                ]))));
+      ),
+    );
+  }
+
+  List<Item> _buildPizzas() {
+    List<Item> lista = [];
+
+    for (var pizza in pizzaData) {
+      lista.add(Item(pizza: pizza));
+    }
+      return lista;
   }
 }
